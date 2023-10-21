88b9ef3176380352c91fccbe36cd2fbb16f21fe0
hiz8 <detroit69city.nau@gmail.com>
2023-10-21 15:21:00 +0900

    Update favicon

--- app/root.tsx
+++ app/root.tsx
@@ -25,6 +25,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <Meta />
         <Links />
       </head>
