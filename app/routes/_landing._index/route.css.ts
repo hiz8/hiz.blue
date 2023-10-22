4ac665c93223fc8b5f7bb5d283a93e7234a5c86b
hiz8 <detroit69city.nau@gmail.com>
2023-10-22 16:48:26 +0900

    Update feeds of top page

--- app/routes/_landing._index/route.css.ts
+++ app/routes/_landing._index/route.css.ts
@@ -7,7 +7,7 @@ export const root = style({
 export const feedItems = style({
   display: "flex",
   flexDirection: "column",
-  gap: vars.space["4x"],
+  gap: vars.space["5x"],
   padding: "0",
 });
 
@@ -41,8 +41,8 @@ export const feedItemContents = style({
 });
 
 export const feedItemTitle = style({
-  fontSize: "18px",
-  fontWeight: 500,
+  fontSize: "24px",
+  fontWeight: 400,
   margin: 0,
 });
 
--- app/routes/_landing._index/route.tsx
+++ app/routes/_landing._index/route.tsx
@@ -1,33 +1,41 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
-import { Link } from "@remix-run/react";
+import { type MetaFunction, json } from "@remix-run/cloudflare";
+import { Link, useLoaderData } from "@remix-run/react";
 
 import { Headline } from "~/components/headline";
 import { Icon } from "~/components/icon";
-
+import { postFromModule } from "~/utils/post-from-module";
 import * as styles from "./route.css";
+
+// All Blog posts
+import * as post001 from "../_landing.blog._detail.20231022.mdx";
 
 export const meta: MetaFunction = () => {
   return [{ title: "hiz" }, { name: "description", content: "Home" }];
 };
 
-const feedItems = [
-  {
-    id: "1",
-    title: "Hello World",
-    domain: "example.com",
-    category: "Blog",
-    href: "/blog/hello-world",
-  },
-  {
-    id: "2",
-    title: "Hello World",
-    domain: "example.com",
-    category: "Blog",
-    href: "/blog/hello-world",
-  },
-];
+export async function loader() {
+  return json([postFromModule(post001)]);
+}
+
+type FieldItem = {
+  id: string;
+  title: string;
+  domain: "hiz.blue";
+  category: "Blog";
+  href: `/blog/${string}`;
+};
 
 export default function Index() {
+  const posts = useLoaderData<typeof loader>();
+
+  const feedItems: FieldItem[] = posts.map((post) => ({
+    id: post.slug,
+    title: post.title,
+    domain: "hiz.blue",
+    category: "Blog",
+    href: `/blog/${post.slug}`,
+  }));
+
   return (
     <div className={styles.root}>
       <section>
