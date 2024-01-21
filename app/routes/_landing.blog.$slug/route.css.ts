9d5f0808578b79c9948338decb53194fff3ef4d7
hiz8 <detroit69city.nau@gmail.com>
2024-01-21 17:55:08 +0900

    Update blog styles

--- app/routes/_landing.blog.$slug/route.css.ts
+++ app/routes/_landing.blog.$slug/route.css.ts
@@ -1,6 +1,9 @@
 import { style, vars, globalStyle } from "~/styles.css";
 
-export const root = style({});
+export const root = style({
+  maxWidth: "40em",
+  lineHeight: 2,
+});
 
 globalStyle(`${root} h1`, {
   fontSize: 24,
@@ -11,4 +14,8 @@ globalStyle(`${root} h2, ${root} h3, ${root} h4, ${root} h5, ${root} h6`, {
   fontSize: 24,
   fontWeight: 500,
   marginBlock: vars.space["2x"],
+});
+
+globalStyle(`${root} p`, {
+  whiteSpace: "pre-wrap",
 });
