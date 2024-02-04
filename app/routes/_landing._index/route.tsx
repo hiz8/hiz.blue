bce67a3b467ba3960de48e823a50a59d307f1611
hiz8 <detroit69city.nau@gmail.com>
2024-02-04 13:31:44 +0900

    Refactor code to use defer and Suspense for data loading

--- app/routes/_landing._index/route.tsx
+++ app/routes/_landing._index/route.tsx
@@ -1,18 +1,15 @@
-import { json } from "@remix-run/cloudflare";
+import { defer } from "@remix-run/cloudflare";
 import type { MetaFunction, LoaderArgs } from "@remix-run/cloudflare";
-import { Link, useLoaderData } from "@remix-run/react";
+import { Await, Link, useLoaderData } from "@remix-run/react";
+import { Suspense } from "react";
 
 import { Headline } from "~/components/headline";
 import { Icon } from "~/components/icon";
 import { Client } from "~/utils/notion";
 import { postFromNotionResponse } from "~/utils/post-from-notion";
-import { Cache } from "~/utils/cache";
+import { Cache, storeData, type Data } from "~/utils/cache";
 
 import * as styles from "./route.css";
-
-type Data = {
-  posts: ReturnType<typeof postFromNotionResponse>[];
-};
 
 export const meta: MetaFunction = () => {
   return [{ title: "hiz" }, { name: "description", content: "Home" }];
@@ -27,12 +24,10 @@ export async function loader({ context, request }: LoaderArgs) {
   }
 
   const client = new Client(context.env.NOTION_API_KEY);
+  const data = client.getDatabase(context.env.NOTION_DATABASE_ID);
+  context.waitUntil(storeData(data, cache));
 
-  const data = await client.getDatabase(context.env.NOTION_DATABASE_ID);
-  const posts = data.map(postFromNotionResponse);
-  context.waitUntil(cache.set({ posts }));
-
-  return json({ posts });
+  return defer({ data });
 }
 
 type FieldItem = {
@@ -44,15 +39,7 @@ type FieldItem = {
 };
 
 export default function Index() {
-  const { posts } = useLoaderData<typeof loader>();
-
-  const feedItems: FieldItem[] = posts.map((post) => ({
-    id: post.id,
-    title: post.title,
-    domain: "hiz.blue",
-    category: "Blog",
-    href: `/blog/${post.slug}`,
-  }));
+  const { data } = useLoaderData<typeof loader>();
 
   return (
     <div className={styles.root}>
@@ -61,25 +48,59 @@ export default function Index() {
           <Icon type="feed" />
           Feed
         </Headline>
-        {feedItems.length === 0 && (
-          <p className={styles.feedItemsEmpty}>No feed items yet.</p>
-        )}
-        <ul className={styles.feedItems}>
-          {feedItems.map((item) => (
-            <Link to={item.href} key={item.id} className={styles.feedItemLink}>
-              <li className={styles.feedItem}>
-                <div className={styles.feedItemImage} />
-                <div className={styles.feedItemContents}>
-                  <h2 className={styles.feedItemTitle}>{item.title}</h2>
-                  <div className={styles.feedItemMeta}>
-                    <p className={styles.feedItemDomain}>{item.domain}</p>
-                    <p className={styles.feedItemCategory}>{item.category}</p>
-                  </div>
-                </div>
-              </li>
-            </Link>
-          ))}
-        </ul>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Await resolve={data}>
+            {(posts) => {
+              if (!Array.isArray(posts)) {
+                return <p>No feed items yet.</p>;
+              }
+
+              const feedItems: FieldItem[] = posts
+                .map(postFromNotionResponse)
+                .map((post) => ({
+                  id: post.id,
+                  title: post.title,
+                  domain: "hiz.blue",
+                  category: "Blog",
+                  href: `/blog/${post.slug}`,
+                }));
+
+              return (
+                <>
+                  {feedItems.length === 0 && (
+                    <p className={styles.feedItemsEmpty}>No feed items yet.</p>
+                  )}
+                  <ul className={styles.feedItems}>
+                    {feedItems.map((item) => (
+                      <Link
+                        to={item.href}
+                        key={item.id}
+                        className={styles.feedItemLink}
+                      >
+                        <li className={styles.feedItem}>
+                          <div className={styles.feedItemImage} />
+                          <div className={styles.feedItemContents}>
+                            <h2 className={styles.feedItemTitle}>
+                              {item.title}
+                            </h2>
+                            <div className={styles.feedItemMeta}>
+                              <p className={styles.feedItemDomain}>
+                                {item.domain}
+                              </p>
+                              <p className={styles.feedItemCategory}>
+                                {item.category}
+                              </p>
+                            </div>
+                          </div>
+                        </li>
+                      </Link>
+                    ))}
+                  </ul>
+                </>
+              );
+            }}
+          </Await>
+        </Suspense>
       </section>
     </div>
   );
