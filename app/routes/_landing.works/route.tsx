b0a6ec24ffa6762b1ebd4f33afc8b2633d248f07
hiz8 <detroit69city.nau@gmail.com>
2024-03-09 15:14:23 +0900

    Add new works and archives to landing page

--- app/routes/_landing.works/route.tsx
+++ app/routes/_landing.works/route.tsx
@@ -13,13 +13,32 @@ export const meta: MetaFunction = () => {
 const works = [
   {
     id: "1",
+    title: "VS Code Ruby Blue Theme",
+    description: "Dark, high contrast theme for VS Code.",
+    url: "https://marketplace.visualstudio.com/items?itemName=hirofumii.rubyblue-theme",
+    image: "/images/icon-ruby-blue-theme.png",
+  },
+  {
+    id: "2",
+    title: "Noto Sans CJK JP min",
+    description: "Subset of the Noto Sans CJK JP for the size down.",
+    url: "https://hiz8.github.io/Noto-Sans-CJK-JP.min/",
+  },
+  {
+    id: "3",
+    title: "Noto Serif CJK JP min",
+    description: "Subset of the Noto Selif CJK JP for the size down.",
+    url: "https://hiz8.github.io/Noto-Serif-CJK-JP.min/",
+  },
+  {
+    id: "4",
     title: "airbeat",
     description: "Offline first metronome application.",
     url: "https://airbeat.hizapp.blue/",
     image: "/images/icon-airbeat.svg",
   },
   {
-    id: "2",
+    id: "5",
     title: "Giji one",
     description:
       "A tool for the assistance of people in the creation of meeting minutes.",
@@ -27,11 +46,44 @@ const works = [
     image: "/images/icon-gijione.svg",
   },
   {
-    id: "3",
+    id: "6",
     title: "Cinemasaurus",
     description: "沖縄県内の映画情報サイト",
     url: "https://cinemasaurus.net/",
     image: "/images/icon-cinemasaurus.svg",
+  },
+];
+
+const arcives = [
+  {
+    id: "1",
+    title: "NAUTILUS OFFICIAL WEBSITE",
+    description: "NAUTILUS の公式ウェブサイト",
+    url: "https://nautilus-jp.com/",
+  },
+  {
+    id: "2",
+    title: "宜野湾 HUMAN STAGE",
+    description: "宜野湾 HUMAN STAGE の公式ウェブサイト",
+    url: "https://www.humanstage.net/",
+  },
+  {
+    id: "3",
+    title: "Playlog",
+    description: "Webフロントエンドについて徒然と",
+    url: "https://log.plyrs.net/",
+  },
+  {
+    id: "4",
+    title: "Playground",
+    description: "Playground for Future of Web Technology.",
+    url: "https://ground.plyrs.net/",
+  },
+  {
+    id: "5",
+    title: "hexo-theme-amp",
+    description: "A simple and mobile first Hexo template on AMP ⚡ HTML.",
+    url: "https://hiz8.github.io/hexo-theme-amp/",
   },
 ];
 
@@ -44,6 +96,13 @@ export default function Index() {
       </Headline>
       <div className={styles.cardHolder}>
         {works.reverse().map((work) => (
+          <Card key={work.id} {...work} />
+        ))}
+      </div>
+
+      <Headline>Legacy/Archived</Headline>
+      <div className={styles.cardHolder}>
+        {arcives.reverse().map((work) => (
           <Card key={work.id} {...work} />
         ))}
       </div>
