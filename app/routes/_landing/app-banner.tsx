a9a317ad87c596960c05f0b4973d2ac99a0921cd
hiz8 <detroit69city.nau@gmail.com>
2024-03-09 10:19:32 +0900

    Add link to headline in app-banner

--- app/routes/_landing/app-banner.tsx
+++ app/routes/_landing/app-banner.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-aria-components";
 import * as styles from "./app-banner.css";
 
 export function AppBanner() {
   return (
     <div className={styles.root}>
-      <h1 className={styles.headline}>hiz</h1>
+      <Link href="/" className={styles.headlineLink}>
+        <h1 className={styles.headline}>hiz</h1>
+      </Link>
       <p className={styles.subHeadline}>UX designer, developer in Japan.</p>
       <span className={styles.cloudOne} />
       <span className={styles.cloudTwo} />
